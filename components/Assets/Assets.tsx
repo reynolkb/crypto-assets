664c438279393a966310b46d457ea82a03772cef
Kyle Reynolds <kyle@parallel.fi>
2022-09-19 13:58:22 -0400

    initial commit

---
+++ components/Assets/Assets.tsx
@@ -0,0 +1,91 @@
+import { Box, Typography, Button, Paper, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent, Grid, styled, IconButton } from '@mui/material';
+import { useState } from 'react';
+import Outbound from '@mui/icons-material/Outbound';
+import { CryptoAsset } from './CryptoAssets';
+
+const assetHeaders = ['Asset', 'Available', 'APY, variable', 'APY, stable'];
+const assetMobileHeaders = ['Asset', 'Available'];
+
+const assets = [
+  {
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    available: '5',
+    apyV: '< 0.01%',
+    apyS: '5.46%',
+  },
+  {
+    name: 'Ethereum',
+    symbol: 'ETH',
+    available: '100',
+    apyV: '< 0.01%',
+    apyS: '5.15%',
+  },
+  {
+    name: 'Cardano',
+    symbol: 'ADA',
+    available: '1,000',
+    apyV: '< 0.01%',
+    apyS: '5.32%',
+  },
+  {
+    name: 'USDC',
+    symbol: 'USDC',
+    available: '10,000',
+    apyV: '< 0.01%',
+    apyS: '4.87%',
+  },
+  {
+    name: 'Tether',
+    symbol: 'USDT',
+    available: '10,000',
+    apyV: '< 0.01%',
+    apyS: '4.91%',
+  },
+];
+
+export interface AssetProps {
+  isSm: boolean;
+  isMd: boolean;
+}
+
+export const Assets: React.FC<AssetProps> = ({ isSm, isMd }) => {
+  return (
+    <Paper elevation={3} sx={{ display: 'flex', flexDirection: 'column', mt: 5, borderRadius: '5px' }}>
+      {isSm ? (
+        <>
+          <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'space-between', pt: 3, pb: 2, pl: 4, pr: 4, alignItems: 'center' }}>
+            <Typography variant="h5">Assets to borrow</Typography>
+          </Box>
+        </>
+      ) : (
+        <>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between', pt: 3, pb: 2, pl: 4, pr: 4, alignItems: 'center' }}>
+            <Typography variant="h5">Assets to borrow</Typography>
+          </Box>
+        </>
+      )}
+      <Box sx={{ flexGrow: 1, pt: 2, pb: 1, pl: 3, pr: 3, display: 'flex', flexDirection: 'column' }}>
+        <Box sx={{ pl: 3, pr: 3 }}>
+          <Grid container>
+            {isMd
+              ? Array.from(assetMobileHeaders).map((assetHeader, index) => (
+                  <Grid item xs={6} key={index}>
+                    <Typography variant="body3">{assetHeader}</Typography>
+                  </Grid>
+                ))
+              : Array.from(assetHeaders).map((assetHeader, index) => (
+                  <Grid item xs={3} key={index}>
+                    <Typography variant="body3">{assetHeader}</Typography>
+                  </Grid>
+                ))}
+          </Grid>
+        </Box>
+        {Array.from(assets).map((asset, index) => (
+          <CryptoAsset key={index} name={asset.name} symbol={asset.symbol} available={asset.available} apyV={asset.apyV} apyS={asset.apyS} isSm={isSm} isMd={isMd} />
+        ))}
+        <Box sx={{ mt: 2 }}></Box>
+      </Box>
+    </Paper>
+  );
+};
---
+++ components/Assets/CryptoAssets.tsx
@@ -0,0 +1,129 @@
+import { Box, Typography, Grid, IconButton, Button } from '@mui/material';
+import Outbound from '@mui/icons-material/Outbound';
+import Image from 'next/image';
+import ADA from '../../images/assets/ADA.svg';
+import BTC from '../../images/assets/BTC.svg';
+import ETH from '../../images/assets/ETH.svg';
+import USDC from '../../images/assets/USDC.svg';
+import USDT from '../../images/assets/USDT.svg';
+import CallMissedOutgoingIcon from '@mui/icons-material/CallMissedOutgoing';
+
+export interface CryptoAssetProps {
+  name: string;
+  symbol: string;
+  available: string;
+  apyV: string;
+  apyS: string;
+  isSm: boolean;
+  isMd: boolean;
+}
+
+export const CryptoAsset: React.FC<CryptoAssetProps> = ({ name, symbol, available, apyV, apyS, isSm, isMd }: CryptoAssetProps) => {
+  return (
+    <Box sx={{ backgroundColor: '#FAFAFA', height: '75px', borderRadius: '5px', mt: 2, display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+      <Box sx={{ pl: 3, pr: 3 }}>
+        {isSm ? (
+          <>
+            <Grid container>
+              <Grid item xs={6}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                  {symbol === 'ADA' && <Image src={ADA} alt="ADA" />}
+                  {symbol === 'BTC' && <Image src={BTC} alt="BTC" />}
+                  {symbol === 'ETH' && <Image src={ETH} alt="ETH" />}
+                  {symbol === 'USDC' && <Image src={USDC} alt="USDC" />}
+                  {symbol === 'USDT' && <Image src={USDT} alt="USDT" />}
+                  {!isSm && (
+                    <Box sx={{ ml: 1, display: 'flex', flexDirection: 'column' }}>
+                      <Typography variant="body1" fontWeight={700}>
+                        {name}
+                      </Typography>
+                      <Typography variant="body2">{symbol}</Typography>
+                    </Box>
+                  )}
+                </Box>
+              </Grid>
+              <Grid item xs={6}>
+                <Box sx={{ display: 'flex', alignItems: 'center', height: '100%', justifyContent: 'space-between' }}>
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography variant="body2">{available}</Typography>
+                    <Typography variant="body2">{apyV}</Typography>
+                  </Box>
+                  <CallMissedOutgoingIcon sx={{ ml: 1 }}></CallMissedOutgoingIcon>
+                </Box>
+              </Grid>
+            </Grid>
+          </>
+        ) : isMd ? (
+          <>
+            <Grid container>
+              <Grid item xs={6}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                  {symbol === 'ADA' && <Image src={ADA} alt="ADA" />}
+                  {symbol === 'BTC' && <Image src={BTC} alt="BTC" />}
+                  {symbol === 'ETH' && <Image src={ETH} alt="ETH" />}
+                  {symbol === 'USDC' && <Image src={USDC} alt="USDC" />}
+                  {symbol === 'USDT' && <Image src={USDT} alt="USDT" />}
+                  <Box sx={{ ml: 1, display: 'flex', flexDirection: 'column' }}>
+                    <Typography variant="body1" fontWeight={700}>
+                      {name}
+                    </Typography>
+                    <Typography variant="body2">{symbol}</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={6}>
+                <Box sx={{ display: 'flex', alignItems: 'center', height: '100%', justifyContent: 'space-between' }}>
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography variant="body2">{available}</Typography>
+                    <Typography variant="body2">{apyV}</Typography>
+                  </Box>
+                  <CallMissedOutgoingIcon></CallMissedOutgoingIcon>
+                </Box>
+              </Grid>
+            </Grid>
+          </>
+        ) : (
+          <>
+            <Grid container>
+              <Grid item xs={3}>
+                <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                  {symbol === 'ADA' && <Image src={ADA} alt="ADA" />}
+                  {symbol === 'BTC' && <Image src={BTC} alt="BTC" />}
+                  {symbol === 'ETH' && <Image src={ETH} alt="ETH" />}
+                  {symbol === 'USDC' && <Image src={USDC} alt="USDC" />}
+                  {symbol === 'USDT' && <Image src={USDT} alt="USDT" />}
+                  <Box sx={{ ml: 1, display: 'flex', flexDirection: 'column' }}>
+                    <Typography variant="body1" fontWeight={700}>
+                      {name}
+                    </Typography>
+                    <Typography variant="body2">{symbol}</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                  <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                    <Typography variant="body2">{available}</Typography>
+                  </Box>
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                  <Typography variant="body2">{apyV}</Typography>
+                </Box>
+              </Grid>
+              <Grid item xs={3}>
+                <Box sx={{ display: 'flex', alignItems: 'center', height: '100%', justifyContent: 'space-between' }}>
+                  <Typography variant="body2">{apyS}</Typography>
+                  <Button type="button" fullWidth variant="contained" sx={{ height: '50px', width: '125px', borderRadius: '5px', ml: 1 }}>
+                    Borrow
+                  </Button>
+                </Box>
+              </Grid>
+            </Grid>
+          </>
+        )}
+      </Box>
+    </Box>
+  );
+};
